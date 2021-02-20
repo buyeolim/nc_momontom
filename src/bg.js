59f746f64dbc4979c6cd7d9e361a19bd9b17bfaf
buyeolim <buyeolim@gmail.com>
2021-02-21 02:14:56 +0900

    Fix bg-img

--- src/bg.js
+++ src/bg.js
@@ -9,10 +9,11 @@ function handleImgLoad() {
 function paintImage(imgNumber) {
   const image = new Image();
 
-  image.src = `/images/${imgNumber + 1}.jpg`;
+  image.src = `images/${imgNumber + 1}.jpg`;
   image.classList.add("bgImage");
   body.prepend(image);
 }
+
 function genRandom() {
   const number = Math.floor(Math.random() * 3);
 
